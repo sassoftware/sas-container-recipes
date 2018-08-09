3d0e6b97509aa5f7c4d70dbf2486f5821323c067
Paul Kent <paul.kent@sas.com>
2018-08-09 17:41:23 -0400

    fix #1 - dynamic version number in index.html

--- all-in-one/files/start.sh
+++ all-in-one/files/start.sh
@@ -41,25 +41,6 @@ sleep 5
 
 
 #
-# Write out a help page to be displayed when browsing port 80
-#
-cat > /var/www/html/index.html <<'EOF'
-<html>
- <h1> SAS Viya 3.3 Docker Container </h1>
- <p> Access the software by browsing to:
- <ul>
-  <li> <b><a href="/SASStudio">/SASStudio</a></b>
-  <li> <b><a href="/RStudio/auth-sign-in">/RStudio</a></b> (Not installed by default)
-  <li> <b><a href="/Jupyter">/Jupyter</a></b>
- </ul> using HTTP on port 80.
-
- <p> If port 80 is forwarded to a different port on the host machine, use the host port instead.
-
- <p> Use the <b>sasdemo</b> / <b>sasDEMO</b> login to access SAS Studio, CAS, and Jupyter.
-</html>
-EOF
-
-#
 # Print out the help message without the HTML tags
 #
 sed 's/<[^>]*>//g' /var/www/html/index.html
