3e5817106ff9672438df42c21e27dfb5f425606f
collinux <collinux@users.noreply.github.com>
2019-11-19 11:35:34 -0500

    Updated Recipe version 19.06.2 -> 19.06.3

--- order.go
+++ order.go
@@ -50,7 +50,7 @@ import (
 )
 
 // RecipeVersion format <year>.<month>.<numbered release>
-const RecipeVersion = "19.06.2"
+const RecipeVersion = "19.06.3"
 
 // SasViyaVersion is used to wget the corresponding sas-orchestration tool version
 // example: 34 = version 3.4
